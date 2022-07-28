ad3026dfa0c46270f0e63b8d0c69966059dbc5ac
taftouf <hamza0maarouf1998@gmail.com>
2022-07-28 17:39:34 +0100

    mongodb setup

--- config/database.php
+++ config/database.php
@@ -96,6 +96,7 @@ return [
         'mongodb' => [
             'driver' => 'mongodb',
             'dsn' => env('DB_DSN'),
+            'host' => env('DB_HOST'),
             'database' => env('DB_DATABASE', 'homestead'),
         ],
     ],
