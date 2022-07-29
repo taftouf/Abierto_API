8bdc671b72b46c2f8807cfae772a22257eb470ba
taftouf <hamza0maarouf1998@gmail.com>
2022-07-29 15:17:42 +0100

    test 24

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,13 +9,12 @@ use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Laravel\Sanctum\HasApiTokens;
 
-class User extends \Jenssegers\Mongodb\Eloquent\Model implements
-    AuthenticatableContract,
-    AuthorizableContract,
-    CanResetPasswordContract
+class User extends \Jenssegers\Mongodb\Eloquent\Model
 {
-    use Authenticatable, Authorizable, CanResetPassword, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     protected $connection = 'mongodb';
     protected $collection = 'users';
