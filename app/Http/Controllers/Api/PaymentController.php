123c2c6ab2abfa26de643d73f641279cd4525059
taftouf <hamza0maarouf1998@gmail.com>
2022-07-29 17:27:50 +0100

    test 27

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -74,7 +74,7 @@ class PaymentController extends Controller
                         'platform' => $agent->platform(),
                         'browser' => $agent->browser(),
                         'languages' => $agent->languages(),
-                        'ip' => Location::get($ip)
+                        'ip' => Location::get()->countryName
                     ]
                 );
                 $res = DB::table('payments')->where('owner',$owner)->orderBy('_id', 'desc')->get();;
