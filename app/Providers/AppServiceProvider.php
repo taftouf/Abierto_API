007f4325179f9d069f460e4cc66275784e745548
taftouf <hamza0maarouf1998@gmail.com>
2022-07-29 16:42:39 +0100

    test 27

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use App\Models\Sanctum\PersonalAccessToken;
+use App\Models\PersonalAccessToken;
 use Illuminate\Foundation\AliasLoader;
 
 class AppServiceProvider extends ServiceProvider
@@ -29,6 +29,6 @@ class AppServiceProvider extends ServiceProvider
         $loader = AliasLoader::getInstance();
 
         // SANCTUM CUSTOM PERSONAL-ACCESS-TOKEN
-        $loader->alias(\Laravel\Sanctum\PersonalAccessToken::class, \App\Models\Sanctum\PersonalAccessToken::class);
+        $loader->alias(\Laravel\Sanctum\PersonalAccessToken::class, \App\Models\PersonalAccessToken::class);
     }
 }
