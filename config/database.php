1a16ba735d282dabd2652eebb8ee4033ebe9589d
taftouf <hamza0maarouf1998@gmail.com>
2022-07-29 12:14:14 +0100

    test 7

--- config/database.php
+++ config/database.php
@@ -95,7 +95,7 @@ return [
         
         'mongodb' => [
             'driver' => 'mongodb',
-            'dsn' => env('DB_DSN'),
+            'dsn' => env('DB_DSN'), 
             'database' => env('DB_DATABASE', 'homestead'),
         ],
     ],
