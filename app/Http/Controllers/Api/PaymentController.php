8490b9cd38f4b582d3aba650eb3a0005cd844dc7
taftouf <hamza0maarouf1998@gmail.com>
2022-07-30 18:06:06 +0100

    store

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -74,8 +74,8 @@ class PaymentController extends Controller
                         'browser' => $agent->browser(),
                         'languages' => $agent->languages(),
                         'location' => Location::get($request->header('X-Forwarded-For')),
-                        'page' => $request->getSchemeAndHttpHost(),
-                        'domain' => $request->fullUrl()
+                        'page' => $request->path(),
+                        'domain' => $request->getHttpHost()
                     ]
                 );
                 $res = DB::table('payments')->where('owner',$owner)->orderBy('_id', 'desc')->first();;
